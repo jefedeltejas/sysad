e85207ac61c3fda6f03c2222940f9afac5c40d6a
jefedeltejas <jeff.jackson@dashiell.com>
2017-06-16 22:32:27 -0500

    experimented with an arp cmd w python

---
+++ python_sysad/pyarp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python   
+#A System Information Gathering Script
+import subprocess
+
+#Command 1
+def arp_func():
+
+    arp = "arp"
+    arp_arg = "-a"
+    stdout = " >> "
+    destination_file = "arpinfo.txt"
+    print "Gathering ARP info from router with %s command:\n" % arp
+    subprocess.call([arp, arp_arg, stdout, destination_file])
+
+#def cat_func():
+
+#    cat = "cat"
+#    cat_file = "arpinfo.txt"
+#    subprocess.call([cat, cat_file])
+
+
+def main():
+    arp_func()
+#    cat_func()
+
+main()
+
+ 
